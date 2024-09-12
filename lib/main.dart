d70f082bf1936e0e7e914aabdd66d10edc07d5fa
Pietro Zardini <pizardini@gmail.com>
2024-09-12 20:06:43 -0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Recebendo número:'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int numero = 0;
+
+  TextEditingController controlador = TextEditingController(); 
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+
+        title: Text(widget.title),
+      ),
+      body: Center(
+
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: TextField(
+	              controller: controlador,
+              ),
+            ),
+            Text(
+
+              'Resultado: $numero',
+              style: const TextStyle(
+                fontSize: 32,
+              ),
+              ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+
+                  numero = int.parse(controlador.text);
+                });
+              },
+              child: const Text('Verificar'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
